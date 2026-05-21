54213712e3ada700f831664fed04f708f841021e
Pradeep-gif-hub <pawasthi063@gmail.com>
2026-05-21 12:38:46 +0530

    change the id and remove few hobbies

--- src/data/hobbies.ts
+++ src/data/hobbies.ts
@@ -7,30 +7,18 @@ export const hobbies = [
   },
   {
     id: 2,
-    label: "Writing",
-    icon: "PenTool",
-    description: "Tech blogs and technical articles",
-  },
-  {
-    id: 3,
     label: "Coffee",
     icon: "Coffee",
     description: "Chai pe Charcha with developers",
   },
   {
-    id: 4,
+    id: 3,
     label: "Music",
     icon: "Music",
     description: "Indie and lo-fi beats",
   },
   {
-    id: 5,
-    label: "Photography",
-    icon: "Camera",
-    description: "Capturing moments and light",
-  },
-  {
-    id: 6,
+    id: 4,
     label: "Gaming",
     icon: "Gamepad2",
     description: "Competitive & casual gaming",
